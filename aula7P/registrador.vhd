36bd067c2d9229107d2e638d055f2a80fac180cd
emanuelle guse <120527386+manuguse@users.noreply.github.com>
2023-09-25 16:49:37 -0300

    Update registrador.vhd

--- aula7P/registrador.vhd
+++ aula7P/registrador.vhd
@@ -4,7 +4,7 @@ use IEEE.Std_Logic_1164.all;
 entity registrador is
 generic(n:integer);
 port (D: in std_logic_vector(n-1 downto 0);  
-	  CLK, RST: in std_logic;
+	  CLK, RST, load: in std_logic;
 	  H: out std_logic_vector(n-1 downto 0));
 end registrador;
 
@@ -12,7 +12,7 @@ architecture registrador of registrador is
 begin 
 	process(CLK, D, RST)
 	begin
-		if (RST = '1') then H <= "0000";
+		if (RST = '1') then H <= (others => '0')
 		elsif (CLK'event and CLK = '1') then H <= D;
 		end if;
 	end process;
