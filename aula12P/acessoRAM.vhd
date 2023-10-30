a63b859832e96cfd5f0d18ed3a35be9404f24e30
emanuelle guse <120527386+manuguse@users.noreply.github.com>
2023-10-30 16:41:25 -0300

    Update acessoRAM.vhd

--- aula12P/acessoRAM.vhd
+++ aula12P/acessoRAM.vhd
@@ -16,7 +16,26 @@ end entity;
 
 architecture estrutura of acessoRAM is
 
-	signal -- completar
+	component controle is
+	port(
+		clock, clear: in std_logic;
+		iniciar: in std_logic;
+		pronto: out std_logic;
+		ctrl1, ctrl2, ctrl3: out std_logic;
+		stt1: in std_logic);
+	end component;
+	
+	component operativo is
+	port(
+		clock, clear: in std_logic;
+		address: out std_logic_vector(7 downto 0);
+		wren: out std_logic;
+		q: in std_logic_vector(7 downto 0)
+		ctrl1, ctrl2, ctrl3: in std_logic;
+		stt1: out std_logic);
+	end component;
+
+	signal ctrl1, ctrl2, ctrl3, stt1: std_logic;
 
 begin
 end architecture;
