10b4597f079d721a0bd35d3cb1c1d6f5eafed148
emanuelle guse <120527386+manuguse@users.noreply.github.com>
2023-12-06 16:38:12 -0300

    Update dataAddress.vhd

--- dataAddress.vhd
+++ dataAddress.vhd
@@ -1,18 +1,17 @@
--- ainda nao pronto
-
 library ieee;
 use ieee.std_logic_1164.all; 
 use ieee.math_real.all;
+--arch
 use ieee.numeric_std.all;
 
 entity dataAddress is
 	generic(	
-	    addressWidth: positive := 10;
-		dataWidth: positive := 16;   -- in bits ; dataNumBytes = dataWidth/8 
+	    addressWidth: positive;
+		dataWidth: positive := 8;   -- in bits ; dataNumBytes = dataWidth/8 
 		isVector: boolean := false; -- vector is a column matrix; vector[column] = matrix[0,column]
 		isMatrix: boolean := true; -- matrix[row,column]
-		numRowElements: positive := 16;  
-		numColumnElements: positive := 64
+		numRowElements: positive := 1;  
+		numColumnElements: positive := 1
 	);
 	port(
 	    baseaddress: in std_logic_vector(addresswidth-1 downto 0);
@@ -27,47 +26,60 @@ end entity;
 
 architecture BEHAVIOURAL of dataAddress is
 	constant dataNumBytes: positive := integer(ceil(real(dataWidth)/8.0));
-   signal add1, add2, add3: std_logic_vector(addressWidth-1 downto 0);
+	signal output: std_logic_vector(addressWidth-1 downto 0);
+	--signal add1, add2, add3: std_logic_vector(addressWidth-1 downto 0);
+    
     -- COMPLETE	
 begin
 	-- general formula for all:  baseaddress + dataNumBytes * (row * numColumnElements + column) + byteToAccess
-    
+
 	-- Same formula simplified for variables with dataNumBytes = 1:  baseaddress
-	var1byte: if not isVector and not isMatrix and dataNumBytes = 1  generate -- funcionando
-	    address <= baseaddress;
+	var1byte: if not isVector and not isMatrix and dataNumBytes = 1  generate
+	    output <= baseaddress;
 	end generate;
 	
 	-- Same formula simplified for variables with dataNumBytes > 1:  baseaddress + byteToAccess
     varP1byte: if not isVector and not isMatrix and dataNumBytes > 1 generate
-        address <= std_logic_vector(unsigned(baseaddress) + unsigned(byteToAccess));
+        output <= std_logic_vector(unsigned(baseaddress) + unsigned(byteToAccess));
     end generate;
     
 	-- Same formula simplified for vectors with dataNumBytes = 1:  baseaddress + column
 	vector1byte: if isVector and not isMatrix and dataNumBytes = 1 generate
-	    address <= std_logic_vector(unsigned(baseaddress) + unsigned(column));
+	    output <= std_logic_vector(unsigned(baseaddress) + unsigned(column));
 	end generate;
 	
 	-- Same formula simplified for vectors with dataNumBytes > 1:  baseaddress + dataNumBytes*column + byteToAccess
-	vectorP1byte: if isVector and not isMatrix and dataNumBytes > 1 generate
-        add1 <= std_logic_vector(unsigned(baseaddress) + unsigned(byteToAccess));
-        add2 <= std_logic_vector((to_unsigned(dataNumBytes, column'length) * unsigned(column)));
-        add3 <= std_logic_vector(unsigned(add1) + unsigned(add2));
-        address <= add3;
-	  end generate;
+    vectorP1byte: if isVector and not isMatrix and dataNumBytes > 1 generate
+    	signal add1, add2, add3: std_logic_vector(addressWidth-1 downto 0);
+        begin
+            add1 <= std_logic_vector(unsigned(baseaddress) + unsigned(byteToAccess));
+            add2 <= std_logic_vector((to_unsigned(dataNumBytes, column'length)*unsigned(column)));
+            add3 <= std_logic_vector(unsigned(add1) + unsigned(add2));
+            output <= add3;
+    end generate;
     
 	-- Same formula simplified for matrixes with dataNumBytes = 1:  baseaddress + row*numColumnElements + column
-	 matrix1byte: if isMatrix and not isVector and dataNumBytes = 1 generate
-        add1 <= std_logic_vector(unsigned(baseaddress) + unsigned(column));
-        add2 <= std_logic_vector((to_unsigned(numColumnElements, row'length)*unsigned(row)) + unsigned(add1));
-        address <= add2;	 
-		  end generate;
+	matrix1byte: if isMatrix and not isVector and dataNumBytes = 1 generate
+	    signal add1, add2: std_logic_vector(addressWidth-1 downto 0);
+        begin
+            add1 <= std_logic_vector(unsigned(baseaddress) + unsigned(column));
+            add2 <= std_logic_vector((to_unsigned(numColumnElements, row'length)*unsigned(row)) + unsigned(add1));
+	        output <= add2;
+	 end generate;
 	
 	-- Same formula simplified for matrixes with dataNumBytes > 1:  baseaddress + dataNumBytes*row*numColumnElements + dataNumBytes*column + byteToAccess
-	matrix1Pbyte: if isMatrix and not isVector and dataNumBytes > 1 generate
-		  add1 <= std_logic_vector(unsigned(baseaddress) + unsigned(byteToAccess));
-        add2 <= std_logic_vector((to_unsigned(dataNumBytes, column'length)*unsigned(column) + unsigned(add1)));
-        add3 <= std_logic_vector(((to_unsigned(dataNumBytes, row'length)*to_unsigned(numColumnElements, row'length)*unsigned(row) + unsigned(add2))));
-        address <= add3;
-	end generate;
 	
+	matrix1Pbyte: if isMatrix and not isVector and dataNumBytes > 1 generate
+    	signal add1, add2, add3: std_logic_vector(addressWidth-1 downto 0);
+        begin
+            add1 <= std_logic_vector(unsigned(baseaddress) + unsigned(byteToAccess));
+            add2 <= std_logic_vector((to_unsigned(dataNumBytes, column'length)*unsigned(column) + unsigned(add1)));
+            add3 <= std_logic_vector((to_unsigned(dataNumBytes, row'length)*to_unsigned(numColumnElements, row'length)*unsigned(row) + unsigned(add2)));
+	        output <= add3;
+	 end generate;
+	
+	-- vector is the same as matrix[0, column]
+	-- variable is the same as matrix[0, 0]	
+
+    address <= output;
 end architecture;
